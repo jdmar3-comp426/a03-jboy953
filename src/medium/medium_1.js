076f46a561cc0a1ec3bdd8f8b129e76a4b919b4f
jboy953 <jboy953@live.unc.edu>
2021-12-04 04:00:46 -0500

    booyah

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -13,9 +13,7 @@ export function getSum(array) {
     for(i=0; i<array.length; i++){
         sum+= array[i];
     }
-    return {
-        sum: sum
-    };
+    return sum;
 }
 
 
@@ -29,7 +27,12 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-
+    array.slice().sort((a,b) => {return a-b});
+    if(array.length % 2 == 0){
+        return (array[length/2] + array[length/2 - 1]) / 2;
+    } else{
+        return array[Math.floor(array.length/2)];
+    }
 }
 
 /**
@@ -52,6 +55,16 @@ export function getMedian(array) {
  }
  */
 export function getStatistics(array) {
-
+    
+    return {
+        length: array.length,
+        sum: getSum(array),
+        mean: (sum / length),
+        median: getMedian(array),
+        min: Math.min(...array),
+        max: Math.max(...array),
+        variance: variance(array, mean),
+        standard_deviation: Math.sqrt(variance),
+    }
 }
 
